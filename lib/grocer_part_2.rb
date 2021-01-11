7f8fd2ca745a53b9a997061f3ba1f021c8036db3
calm-collection-3916 <khalinredd@gmail.com>
2021-01-11 02:40:43 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -30,13 +30,7 @@ def apply_clearance(cart)
   # Consult README for inputs and outputs
   #
   # REMEMBER: This method **should** update cart
-    cart.map do |cart_item|
-    if cart_item[:clearance]
-      cart_item[:price] *= 0.80
-      cart_item[:price].round(2)
-    end
-  end
-  cart
+
 end
 
 def checkout(cart, coupons)
@@ -49,13 +43,5 @@ def checkout(cart, coupons)
   #
   # BEFORE it begins the work of calculating the total (or else you might have
   # some irritated customers
-  new_cart = consolidate_cart(cart)
-  couped_cart = apply_coupons(new_cart, coupons)
-  total = apply_clearance(couped_cart).sum do |e|
-    (e[:price] * e[:count]).round(2)
-  end
-  if total > 100
-    (total = total *= 0.90).round(2)
-  end
-  total
+
 end
