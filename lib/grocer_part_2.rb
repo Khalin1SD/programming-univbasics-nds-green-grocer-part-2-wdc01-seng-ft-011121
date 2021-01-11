ce12eb0e58e57f5fd9ccd55599d783cdccae1cd5
calm-collection-3916 <khalinredd@gmail.com>
2021-01-11 02:28:01 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -4,7 +4,14 @@ def apply_coupons(cart, coupons)
   # Consult README for inputs and outputs
   #
   # REMEMBER: This method **should** update cart
-  
+    coupon_cart = []
+  cart.each do |cart_item|
+    coupons.each do |coupon_item|
+     if cart_item[:item] == coupon_item[:item] && cart_item[:count] >= coupon_item[:num]
+      discounted_item = {:item => "#{cart_item[:item]} W/COUPON", :price => coupon_item[:cost] / coupon_item[:num], :clearance => cart_item[:clearance], :count => coupon_item[:num]}
+      cart_item[:count] = cart_item[:count] - coupon_item[:num]
+      if  cart_item[:count] > 0
+        remainder_it
   
 end
 
